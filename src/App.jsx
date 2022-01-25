ddec98293a341c35ea405e40dca0e662a9bb62df
Mark Voltaire <markjvoltaire@gmail.com>
2022-01-25 15:52:52 -0500

    one fetch written

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import { useEffect } from 'react';
+import { fetchPinBalls } from './services/promise-me';
+
 export default function App() {
+  useEffect(() => {
+    fetchPinBalls();
+  });
   return <h1>Hello World</h1>;
 }
--- src/services/promise-me.js
+++ src/services/promise-me.js
@@ -2,6 +2,17 @@
  * TODO:
  */
 
+export function fetchPinBalls() {
+  const pinBalls = fetch(`https://ponball.herokuapp.com/api/v1/machines`)
+    .then((data) => data.json())
+    .then((newData) => {
+      console.log(newData);
+      return newData;
+    });
+
+  return pinBalls;
+}
+
 /**
  * TODO:
  */
